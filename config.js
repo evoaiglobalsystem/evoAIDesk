2ef89479d3b9bdfa785871a9fdc8bb251ce545f5
aiimplementer <ai.implementer@gmail.com>
2026-04-24 17:38:12 +0530

    initial commit

---
+++ config.js
@@ -0,0 +1,7 @@
+const API = {
+  n8n_trigger_email_workflow: "https://n8n.evoaiglobal.ai/webhook/evoaidesk_trigger_email",  
+  n8n_daily_report_workflow: "https://n8n.evoaiglobal.ai/webhook/evoAIDesk_daily_report",
+  n8n_weekly_report_workflow: "https://n8n.evoaiglobal.ai/webhook/evoAIDesk_weekly_report",
+  n8n_summary_report_workflow: "https://n8n.evoaiglobal.ai/webhook/evoAIDesk_summary_report",
+  n8n_activity_pipeline: "https://n8n.evoaiglobal.ai/webhook/evoaidesk_activities"
+};
