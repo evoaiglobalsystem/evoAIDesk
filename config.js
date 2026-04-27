3b2a91db94b08a1adf23728f6249b9c2ea01a5fb
evoAI <evoaiglobal.system@gmail.com>
2026-04-27 16:04:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const API = {
-  n8n_trigger_email_workflow: "https://n8n.evoaiglobal.ai/webhook-test/evoaidesk_trigger_email",  
+  n8n_trigger_email_workflow: "https://n8n.evoaiglobal.ai/webhook/evoaidesk_trigger_email",  
   n8n_daily_report_workflow: "https://n8n.evoaiglobal.ai/webhook/evoAIDesk_daily_report",
   n8n_weekly_report_workflow: "https://n8n.evoaiglobal.ai/webhook/evoAIDesk_weekly_report",
   n8n_summary_report_workflow: "https://n8n.evoaiglobal.ai/webhook/evoAIDesk_summary_report",
