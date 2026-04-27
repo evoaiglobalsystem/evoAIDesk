cd0a382f78e5455f6cbf21b12ad1200915e49ae2
evoAI <evoaiglobal.system@gmail.com>
2026-04-27 13:53:15 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const API = {
   n8n_daily_report_workflow: "https://n8n.evoaiglobal.ai/webhook/evoAIDesk_daily_report",
   n8n_weekly_report_workflow: "https://n8n.evoaiglobal.ai/webhook/evoAIDesk_weekly_report",
   n8n_summary_report_workflow: "https://n8n.evoaiglobal.ai/webhook/evoAIDesk_summary_report",
-  n8n_company_summary_report_workflow: "https://n8n.evoaiglobal.ai/webhook/evoAIDesk_company_summary_report",
+  n8n_company_summary_report_workflow: "https://n8n.evoaiglobal.ai/webhook-test/evoAIDesk_company_summary_report",
   n8n_activity_pipeline: "https://n8n.evoaiglobal.ai/webhook/evoaidesk_activities",
   n8n_company_intel_webhook: 'https://n8n.evoaiglobal.ai/webhook/evoaidesk_company_intel'
 };
